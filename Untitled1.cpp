257c9bada686c2fcad4a25edfce8a90f2e0c8715
WajihaSajid030 <wajihasajid058@gmail.com>
2025-06-24 12:41:15 +0500

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string name;
+	int age;
+	float gpa;
+	cout<<"Enter name\n";
+	cin>>name;
+	cout<<"Enter age\n";
+	cin>>age;
+	cout<<"Enter gpa\n";
+	cin>>gpa;
+	cout<<"The name is:"<<name<<endl;
+	cout<<"The age is:"<<age<<endl;
+	cout<<"The gpa is:"<<gpa<<endl;
+	return 0;
+	
+}
